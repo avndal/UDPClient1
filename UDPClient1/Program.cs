0b317bafd893ad56436826def9bf298b53e0626c
Rasmus Avndal Hansen <rah001@edu.zealand.dk>
2024-09-12 10:51:24 +0200

    Simpel UDP Client

--- UDPClient1/Program.cs
+++ UDPClient1/Program.cs
@@ -1,2 +1,15 @@
 ﻿
+using System.Net.Sockets;
+using System.Text;
+
 Console.WriteLine("UDP Client");
+
+using (UdpClient socket = new UdpClient())
+{
+    Console.WriteLine("indtast besked");
+
+    string message = Console.ReadLine();
+    byte[] data = Encoding.UTF8.GetBytes(message);
+    socket.Send(data, data.Length, "127.0.0.1", 5005);
+
+}
